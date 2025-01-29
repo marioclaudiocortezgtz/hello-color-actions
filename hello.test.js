1141233dcbff6a6c15a3f6a6840de04fa564e8d0
marioclaudiocortezgtz <mario.cortez@grupotimexico.com.mx>
2025-01-28 22:19:19 -0600

    hello.test.js updated hello message

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from the Office Hours in the terminal!");
+    expect(hello.hello()).toEqual("Hello World I am Mario Cortez from Monterrey city in the north of Mexico");
   });
 });
