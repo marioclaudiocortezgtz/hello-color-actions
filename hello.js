f0f74dd89c22e9b57d142a9fe3b8f77c43594b13
marioclaudiocortezgtz <mario.cortez@grupotimexico.com.mx>
2025-01-28 22:14:10 -0600

    const hello message changes according requirement

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World I am Mario Cortez from Monterrey city in the north of Mexico";
 console.log(hello());
 exports.hello = hello;
